5a021ba2644bb29ebcc5cf6435a32384ba8df337
matekuor <matekuor94@gmail.com>
2019-10-22 01:01:21 +0100

    Update apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-//const API_URL = 'https://hmsbackend.herokuapp.com/api';
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://hmsbackend.herokuapp.com/api';
+//const API_URL = 'http://localhost:4000/api';
 
 export class apiService{
 
